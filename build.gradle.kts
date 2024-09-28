d5db6ed9130ab3a5c26e35cd5ef4dff715a4c560
Yacoub sarsam <Yacoub.sarsam@gmail.com>
2024-09-28 22:44:38 +0200

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.4"
+	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.4"
 	application
 }
