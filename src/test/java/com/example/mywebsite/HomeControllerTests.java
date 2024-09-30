22e1a7b3fd614c879816f33ce1b3fffa66755246
Yacoub sarsam <Yacoub.sarsam@gmail.com>
2024-09-30 20:50:18 +0200

    test när test misslyckas

--- src/test/java/com/example/mywebsite/HomeControllerTests.java
+++ src/test/java/com/example/mywebsite/HomeControllerTests.java
@@ -46,30 +46,4 @@ public class HomeControllerTests {
                 .andExpect(MockMvcResultMatchers.view().name("mittcv"));
     }
 
-
-
-    /*@Test
-    public void shouldReturnMinKodPage_whenGetRequestKod() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/minkod"))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("kod"));
-    }
-
-
-    @Test
-    public void testMinaprojekt() throws Exception {
-        // Arrange: Mock the ProjectService response
-        List<ProjectDto> allProjectsDto = Arrays.asList(
-                new ProjectDto(1,"Project 1", new URL("https://yacobsarsam.github.io/Store/html/index.html"),"Description 1"),
-                new ProjectDto(2, "Project 2",new URL("https://yacobsarsam.github.io/Store/html/index.html"), "Description 2")
-        );
-        when(projectService.getAllProjectsDto()).thenReturn(allProjectsDto);
-
-        // Act & Assert: Perform a GET request to "/minkod" and verify the result
-        mockMvc.perform(get("/min-kod"))
-                .andExpect(status().isOk()) // Expect a 200 OK status
-                .andExpect(view().name("kod")) // Expect the view name to be "kod"
-                .andExpect(model().attributeExists("prList")) // Ensure the model contains "prList"
-                .andExpect(model().attribute("prList", allProjectsDto)); // Ensure "prList" contains the mocked projects
-    }*/
 }
