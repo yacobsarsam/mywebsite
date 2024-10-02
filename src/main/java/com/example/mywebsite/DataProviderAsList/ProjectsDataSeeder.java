ac027db7474e3cfd46d26ca3a37b3fe08cb53392
Yacoub sarsam <Yacoub.sarsam@gmail.com>
2024-10-02 21:06:43 +0200

    lagt till project till mina projekt

--- src/main/java/com/example/mywebsite/DataProviderAsList/ProjectsDataSeeder.java
+++ src/main/java/com/example/mywebsite/DataProviderAsList/ProjectsDataSeeder.java
@@ -15,20 +15,21 @@ public class ProjectsDataSeeder {
     public List<Project> GetProjectsAsList() throws MalformedURLException {
         Project p1 = new Project("MyWebsite",
                 new URL("https://github.com/yacobsarsam/mywebsite"),
-                " Webapplikkatin som jag byggt där DevOps metodik används, dvs CI/CD Pipeline, Docker, Tests, Databas och Deployment. " +
+                " Webbapplikation (denna webbsite/applikation) som jag byggt där DevOps metodik används, dvs CI/CD Pipeline, Docker, Tests, Databas och Deployment. " +
                         "Webapplikationen visar mitt CV, källkod och besrivning till några av mina " +
                         "projekt i Github och visar några spelare som hämtas från postgress databas i Render");
         Project p2 = new Project("Shoping cart - Frontend",
                 new URL("https://yacobsarsam.github.io/Store/html/index.html"),
-                "Web Storage (website-frontend), lägga till produkter och varukorg.");
+                "Web Storage (website-frontend), lägga till produkter i varukorg samt varukorg.");
         Project p3 = new Project("Pensionat-application-Backend-course-1-2",
                 new URL("https://github.com/yacobsarsam/Pensionat-application-Backend-course-1-2"),
                 "Pensionat applikation, grupparbete. Funktioner: Inloggning med behörigheter och roller, API:er, " +
                         ", rumbokningar med av- ovh omboknings möjligheter samt mejlbekräftelse, köer och annat");
-        Project p4 = new Project("Pensionat-application-Backend-course-1-2",
+        Project p4 = new Project("Kalender applikation",
                 new URL("https://github.com/yacobsarsam/Kalender-VG"),
-                "Pensionat applikation, grupparbete. Funktioner: Inloggning med behörigheter och roller, API:er, " +
-                        ", rumbokningar med av- och omboknings möjligheter samt mejlbekräftelse, köer och annat");
+                "Kalender applikation. Swing använts som user interface. Möjlighet att viisa kalendern i olika" +
+                        " vyn (månadsvy, veckovy och dagsvy. Dessutom kan man visa inlägg i form av påminnelser, dagbok " +
+                        "och adressbok");
 
         return List.of(p1, p2, p3, p4);
     }
