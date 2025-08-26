1d8dd64bf53e7d14cf95dddf09512ae6f13fb9f3
Yacoub Sarsam <yacoub.sarsam@digitalengine.se>
2025-08-26 22:25:30 +0200

    add NoOpPlayerRepo

---
+++ src/main/java/com/example/mywebsite/Repositories/NoOpPlayerRepo.java
@@ -0,0 +1,4 @@
+package com.example.mywebsite.Repositories;
+
+public class NoOpPlayerRepo implements PlayerRepo {
+}
--- src/main/java/com/example/mywebsite/Repositories/PlayerRepo.java
+++ src/main/java/com/example/mywebsite/Repositories/PlayerRepo.java
@@ -3,5 +3,5 @@ package com.example.mywebsite.Repositories;
 import com.example.mywebsite.Models.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlayerRepo extends JpaRepository<Player, Long> {
+public interface PlayerRepo  {
 }
